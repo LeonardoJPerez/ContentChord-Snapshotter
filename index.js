21c3f2c87b9379ff2c40d8eb793ef87521ab8e69
Leonardo Perez <find.leonardo@gmail.com>
2017-04-21 16:14:56 -0400

    Fixed typo.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const mqOptions = {
     // username: 'nbowman',
     // password: 'password',
     reconnectTime: 1000,
-    chunkSize = 2
+    chunkSize: 2
 };
 
 console.log('Connecting to ' + mqOptions.host);
@@ -20,15 +20,7 @@ const messageHandler = (msg) => {
     console.log('Processing URL ' + msg.content.toString());
 
     const jsonMessage = JSON.parse(msg.content.toString());
-    const url = "https://www.walmart.com/ip/FurReal-Friends-JJ-My-Jumpin-Pug-Pet/42208804"; // Test purposes
     snapshot(jsonMessage.Uri, uuidV4(), (err) => console.log(err || "Success"));
 };
 
-const testMessageHandler = (msg) => {
-    console.log('Processing URL ' + msg.content.toString());
-
-    const url = "https://www.walmart.com/ip/FurReal-Friends-JJ-My-Jumpin-Pug-Pet/42208804"; // Test purposes
-    snapshot(url, uuidV4(), (err) => console.log(err || "Success"));
-};
-
-ampqConnector.start(mqOptions, testMessageHandler);
+ampqConnector.start(mqOptions, messageHandler);
